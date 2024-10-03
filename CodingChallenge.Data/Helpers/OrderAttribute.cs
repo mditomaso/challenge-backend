b0f0977a5c5cb30e429a0ee7453582c7c265ce88
Melina Di Tomaso <melinaditomaso@hotmail.com>
2024-10-03 18:36:42 -0300

    Add custom order attribute

---
+++ CodingChallenge.Data/Helpers/OrderAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CodingChallenge.Data.Helpers
+{
+    public class OrderAttribute : Attribute
+    {
+        public readonly int Order;
+
+        public OrderAttribute(int order)
+        {
+            Order = order;
+        }
+    }
+}
