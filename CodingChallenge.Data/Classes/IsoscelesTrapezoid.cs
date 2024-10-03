2516892e0ffa1331a32aaaa4e86d34cfd803a475
Melina Di Tomaso <melinaditomaso@hotmail.com>
2024-10-03 18:35:54 -0300

    Add new implementation for isosceles trapezoid

---
+++ CodingChallenge.Data/Classes/IsoscelesTrapezoid.cs
@@ -0,0 +1,29 @@
+﻿using CodingChallenge.Data.Helpers;
+using System;
+
+namespace CodingChallenge.Data.Classes
+{
+    [Order(4)]
+    public class IsoscelesTrapezoid : Shape
+    {
+        private decimal _majorBase;
+        private decimal _smallerBase;
+
+        public IsoscelesTrapezoid(decimal majorBase, decimal smallerBase, decimal side) : base(side) 
+        {
+            _majorBase = majorBase;
+            _smallerBase = smallerBase;
+        }
+
+        public override decimal GetArea()
+        {
+            decimal height = (decimal)Math.Sqrt(Math.Pow((double)_side, 2) - (Math.Pow((double)(_majorBase - _smallerBase), 2) / 4));
+            return (_majorBase + _smallerBase) * height / 2;
+        }
+
+        public override decimal GetPerimeter()
+        {
+            return _majorBase + _smallerBase + (2 * _side);
+        }
+    }
+}
